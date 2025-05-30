c40f45421d9467f05ab382dda68420ce6d5c1490
Tomasz Wojdat <tpwojdat@gmail.com>
2025-05-30 19:57:48 +0200

    Allow installing this fork with homebrew

--- packaging/vobsub2srt.rb
+++ packaging/vobsub2srt.rb
@@ -1,11 +1,11 @@
 # Homebrew Formula for VobSub2SRT
-# Usage: brew install https://github.com/ruediger/VobSub2SRT/raw/master/vobsub2srt.rb
+# Usage: brew install https://github.com/tpwo/VobSub2SRT/raw/master/vobsub2srt.rb
 
 require 'formula'
 
 class Vobsub2srt < Formula
-  head 'git://github.com/ruediger/VobSub2SRT.git', :using => :git
-  homepage 'https://github.com/ruediger/VobSub2SRT'
+  head 'git://github.com/tpwo/VobSub2SRT.git', :using => :git
+  homepage 'https://github.com/tpwo/VobSub2SRT'
 
   depends_on 'cmake'
   depends_on 'tesseract'
